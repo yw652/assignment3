071f6a04ab1bf623b7ed3ed6f5b0a3aeb93220e0
yw652 <yw652@nyu.edu>
2014-09-24 18:21:36 -0400

    assignment3

---
+++ yw652/my_package/hello_world.py
@@ -0,0 +1,15 @@
+'''
+Created on Sep 24, 2014
+
+@author: ds-ga-1007
+
+def hello_world():
+    print "Hello World!"
+
+'''
+def hello_world():
+    print "Hello World!"
+
+if __name__ == '__main__':
+    import sys
+    hello_world()
